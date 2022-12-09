0af721fee39549e34755fc87fcdabaeb7a2f545b
ruben00001 <a.ruben00001@gmail.com>
2022-12-09 18:29:27 +0000

    test image

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,6 +4,9 @@ import { defineConfig } from 'astro/config';
 import preact from "@astrojs/preact";
 
 // https://astro.build/config
+import image from "@astrojs/image";
+
+// https://astro.build/config
 export default defineConfig({
-  integrations: [preact()]
+  integrations: [preact(), image()]
 });
